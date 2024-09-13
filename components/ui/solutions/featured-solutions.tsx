141424db3c0a1aa5625289699c33337b0994b16e
francky <francky@tuta.io>
2024-09-13 07:04:58 +0200

    prepare layout

--- components/ui/solutions/featured-solutions.tsx
+++ components/ui/solutions/featured-solutions.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 import parse from 'html-react-parser'
 import { Solution } from "@/lib/cpt-types"
 import FeaturedImage from "./featured-image"
+import { Button } from "../button"
 
 type FeaturedSolutionsProps = {
     solutions: Solution[]
@@ -12,12 +13,13 @@ export default function FeaturedSolutions(props: FeaturedSolutionsProps) {
     const solutions = featuredSolutions.sort((a, b) => a.meta.display_order - b.meta.display_order)
 
     return(
-        <div className="my-4 flex flex-col gap-4 bg-gray-500">
+        <div className="my-4 flex flex-col gap-4">
             <div className="w-full h-[460] relative bg-purple-300" key={solutions[0].id}>
                 <div className="mx-auto max-w-5xl flex p-6 sm:p-8 relative z-10">
                     <div className="w-1/2">
                         <p>{solutions[0].title.rendered}</p>
                         {parse(solutions[0].excerpt.rendered)}
+                        <Button>Dowiedź się więcej</Button>
                     </div>
                     <div className="w-1/2">
                         <FeaturedImage
@@ -35,34 +37,38 @@ export default function FeaturedSolutions(props: FeaturedSolutionsProps) {
                 />
             </div>
 
-            <div className="bg-rose-300">
-                <div className="mx-auto max-w-5xl p-6 sm:p-8 flex gap-4">
-                    <div className="w-1/2 h-[460] relative bg-gray-200" key={solutions[1].id}>
-                        <div className="relative z-10">
+            <div className="relative h-[460] flex justify-center">
+                <div className="absolute z-10 mx-auto max-w-5xl p-6 sm:p-8 flex gap-4">
+                    <div className="w-1/2 h-[460]" key={solutions[1].id}>
+                        <div className="">
                             <p>{solutions[1].title.rendered}</p>
                             {parse(solutions[1].excerpt.rendered)}
+                            <Button>Dowiedź się więcej</Button>
                         </div>
-                        <Image
-                            className="absolute z-0 inset-0 h-full"
-                            alt={solutions[1].title.rendered}
-                            src={solutions[1].meta.bg_img}
-                            width={640}
-                            height={460}
-                        />
                     </div>
-                    <div className="w-1/2 h-[460] relative bg-gray-200" key={solutions[2].id}>
-                        <div className="relative z-10">
+                    <div className="w-1/2 h-[460]" key={solutions[2].id}>
+                        <div className="">
                             <p>{solutions[2].title.rendered}</p>
                             {parse(solutions[2].excerpt.rendered)}
+                            <Button>Dowiedź się więcej</Button>
                         </div>
-                        <Image
-                            className="absolute z-0 inset-0 h-full"
-                            alt={solutions[2].title.rendered}
-                            src={solutions[2].meta.bg_img}
-                            width={640}
-                            height={460}
-                        />
                     </div>
+                </div>
+                <div className="absolute z-0 flex justify-between gap-4 h-full w-full">
+                    <Image
+                        className="inset-0 grow h-full"
+                        alt={solutions[1].title.rendered}
+                        src={solutions[1].meta.bg_img}
+                        width={640}
+                        height={460}
+                    />
+                    <Image
+                        className="inset-0 grow h-full"
+                        alt={solutions[2].title.rendered}
+                        src={solutions[2].meta.bg_img}
+                        width={640}
+                        height={460}
+                    />
                 </div>
             </div>
         </div>
