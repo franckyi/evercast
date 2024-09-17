94dd2d810752c7e8518b107bfda5ba25f2417bf8
francky <francky@tuta.io>
2024-09-17 20:14:36 +0200

    add contacts

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,7 +1,20 @@
-export default function Page() {
+import { getOfficeBySlug } from "@/lib/cpt-service";
+
+export default async function Page() {
+    const office = await getOfficeBySlug('polnocna')
+
     return (
         <div>
-            <h1>Kontakt</h1>
+            <h1>evercast</h1>
+            <div>{office.meta.address}</div>
+            <div>{office.meta.phone}</div>
+            <div>{office.meta.email}</div>
+            <div>{office.meta.hours}</div>
+            <div>{office.meta.company_name}</div>
+            <div>{office.meta.registered_address}</div>
+            <div>{office.meta.krs}</div>
+            <div>{office.meta.nip}</div>
+            <div>{office.meta.regon}</div>
         </div>
     );
 }
