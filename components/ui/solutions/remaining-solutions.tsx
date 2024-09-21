1f66b26796397c922d3b14b5eb052cebfa435914
francky <francky@tuta.io>
2024-09-21 22:53:57 +0200

    fix spacing

--- components/ui/solutions/remaining-solutions.tsx
+++ components/ui/solutions/remaining-solutions.tsx
@@ -19,7 +19,7 @@ export default function RemainingSolutions(props: FeaturedSolutionProps) {
             {orderedSolutions.map((solution: Solution) => {
                 return(
                     <div key={solution.id} className="solution-container w-full md:w-[calc(50%-.5rem)] relative min-h-[460px] flex dark:bg-black">
-                        <div className="content-wrapper absolute h-[460] z-10 p-8 flex gap-4">
+                        <div className="content-wrapper absolute h-full z-10 p-8 flex gap-4">
                             <div className="w-2/3 h-full flex flex-col gap-4">
                                 <p className={`text-4xl text-muted-foreground font-bold ${fontSecondary.className}`}>
                                     {solution.title.rendered}
@@ -27,7 +27,7 @@ export default function RemainingSolutions(props: FeaturedSolutionProps) {
                                 <div className={`text-4xl text-accent font-bold ${fontSecondary.className}`}>
                                     {parse(solution.excerpt.rendered)}
                                 </div>
-                                <Link href={`/rozwiazanie/${solution.id}`} className="btn-link mt-auto" title={solution.title.rendered}>
+                                <Link href={`/rozwiazanie/${solution.id}`} className="mt-auto" title={solution.title.rendered}>
                                     <Button className="w-[200] bg-gradient-to-r from-[#E7411B] to-[#B70D18] hover:from-[#B70D18] hover:to-[#B70D18]" size="lg">Dowiedź się więcej
                                         <Image className="ml-2" src="/arrow-right-white.svg" width={16} height={16} alt="arrow right icon" />
                                     </Button>
