9d13d4095187dd1c49a35b73398d1f4e238ac143
francky <francky@tuta.io>
2024-09-26 18:37:13 +0200

    style hero text box

--- components/ui/hero-section.tsx
+++ components/ui/hero-section.tsx
@@ -38,7 +38,7 @@ export default function HeroSection({hero, targetPage, solution, bg}: HeroSectio
 
             <div
                 className={
-                    `mx-auto max-w-5xl px-8 py-16 flex flex-wrap gap-8 lg:flex-nowrap justify-between
+                    `mx-auto max-w-5xl px-8 py-16 flex flex-wrap items-center gap-8 lg:flex-nowrap justify-between
                     ${isSolution ?  "absolute top-0 left-0 right-0" : ""}`
                     }
             >
@@ -61,13 +61,13 @@ export default function HeroSection({hero, targetPage, solution, bg}: HeroSectio
                 </div>
 
                 {isHome &&
-                    <div className="md:w-1/3 px-4 py-8 flex flex-col gap-4 text-md border b-2 border-muted rounded-sm">
+                    <div className="announce-container md:w-1/3 px-4 py-8 h-fit flex flex-col justify-center gap-4 text-md border b-2 border-muted rounded-sm">
                         <div className={`text-3xl text-white ${fontSecondary.className}`}>
                             {hero.meta.announce_title}
                         </div>
                         <p className="pr-4">
                             {hero.meta.announce_desc}
-                            <Image className="float-right" src="/arrow-right-red.svg" width={16} height={16} alt="arrow right icon" />
+                            <Image className="announce-icon float-right" src="/arrow-right-red.svg" width={16} height={16} alt="arrow right icon" />
                         </p>
                     </div>
                 }
