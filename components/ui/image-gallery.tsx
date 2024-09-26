6a328afc22121f488a4f53bcb20ba593351f384b
francky <francky@tuta.io>
2024-09-26 18:27:36 +0200

    add key

--- components/ui/image-gallery.tsx
+++ components/ui/image-gallery.tsx
@@ -13,9 +13,9 @@ export default function ImageGallery({gallery}: ImageGalleryProps) {
         <div className="gallery mt-8 lg:mt-0 flex flex-wrap lg:flex-nowrap gap-4 justify-start lg:justify-between items-center">
             {images.map( image => {
                 return (
-                    <>
+                    <div key={image}>
                         {parse(image)}
-                    </>
+                    </div>
                 )
             })}
         </div>
